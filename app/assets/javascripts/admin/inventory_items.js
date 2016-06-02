98cff0912d8c3253553f9e768b4ade07e06d27da
ImmoveableOne <achyut@vinsol.com>
2016-06-02 18:35:33 +0530

    In js use !==.

--- app/assets/javascripts/admin/inventory_items.js
+++ app/assets/javascripts/admin/inventory_items.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('[data-behaviour=change_quantity]').on('ajax:success', function(event, data, status, xhr) {
     $tr = $(event.target).closest("tr");
     quantity_display = $tr.children("td[data-class=quantity]");
-    if ((data.status === "success") && quantity_display.text() != data.qty) {
+    if ((data.status === "success") && quantity_display.text() !== data.qty) {
       quantity_display.text(data.qty);
       quantity_display.effect("highlight", {}, 1500);
       $(event.target).find('input[type=text]').val("");
